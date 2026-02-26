97b8eb3241878f7b276efb1a022b04f7797fbaa0
darshi <darshi@DESKTOP-KEAJKMN>
2026-02-26 19:19:36 +0530

    added new package

---
+++ Framework1.0/src/test/java/sample/DemoTest.java
@@ -0,0 +1,17 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	
+	@Test
+	public void Demo1Test() {
+		System.out.println("hello");
+		
+		System.out.println(System.getProperty("name"));
+		
+		System.out.println("bye");
+
+	}
+
+}
